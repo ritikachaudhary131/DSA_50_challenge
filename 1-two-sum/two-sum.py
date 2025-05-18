39908616f6c2ef8bde20adb82643dcda1c66f66a
ritikachaudhary131 <ritika.chaudhary.cseiot.2022@miet.ac.in>
2025-05-18 16:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (97.70%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    """sum of two numbers"""
+    def twoSum(self, nums, target):
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        
