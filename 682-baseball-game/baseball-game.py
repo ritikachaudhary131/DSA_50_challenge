30675c70500e885f4bec2d1359af4ce2749f5101
ritikachaudhary131 <ritika.chaudhary.cseiot.2022@miet.ac.in>
2025-05-21 15:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (38.80%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        
+        lst = []
+
+        for i in operations:
+            if i == 'C': #cancel toh pop out krdo last mai s
+                lst.pop()
+            elif i == 'D':
+                lst.append(lst[-1]*2)#last element ko double krke list m add krdo
+
+            elif i == '+':
+                lst.append(lst[-1]+lst[-2])#score add krdo second last tk fr append
+            else:
+                lst.append(int(i))#last ko show krdo
+        return sum(lst)
