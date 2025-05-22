f82ef17243fc1892722725d3f8a4e51365f51dc8
ritikachaudhary131 <ritika.chaudhary.cseiot.2022@miet.ac.in>
2025-05-22 16:06:54 +0530

    Update baseball-game.py

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,17 +1,15 @@
 class Solution:
-    def calPoints(self, operations: List[str]) -> int:
-        
-        
-        lst = []
+    def calPoints(self, operations=list[int])->int:
+
+        lst=[]
 
         for i in operations:
-            if i == 'C': #cancel toh pop out krdo last mai s
-                lst.pop()
-            elif i == 'D':
-                lst.append(lst[-1]*2)#last element ko double krke list m add krdo
-
-            elif i == '+':
-                lst.append(lst[-1]+lst[-2])#score add krke show krdo 
-            else:
-                lst.append(int(i))#string ko int m convert krke chrd do
-        return sum(lst)
+          if i=="+":
+             lst.append(lst[-1]+lst[-2])#add
+          elif i=="C":
+             lst.pop()#delete
+          elif i=="D":
+              lst.append(lst[-1]*2)#double
+          else :
+            lst.append(int(i))#last m convert string to int agr bch raha h kch
+        return sum(lst)#sbka sum 
